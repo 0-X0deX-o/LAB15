b4030e3a77f8758e524894a1db5621504142015f
Rage <91225758+0-X0deX-o@users.noreply.github.com>
2022-12-02 08:07:36 -0600

    I seriously hate you more than any other human being in all of history

--- compute_grades.py
+++ compute_grades.py
@@ -1,3 +1,6 @@
+# David Liddle              # 12-1-22
+# Lab 15 Exercise 1
+
 outputHead = ['Student ID', 'Name', 'Total Scores/Assignments', 'Total Score', 'Average Score']
 
 def openPrintList(file):
@@ -11,13 +14,6 @@ list1 = openPrintList('students.txt')
 list2 = openPrintList('scores.txt')
 print(list1,list2)
 
-for element in list2:
-    print(element)
-
-for element in list1:
-    print(element)
-
-
 class Student:
 
     def __init__(self, id, name):
@@ -26,13 +22,48 @@ class Student:
         self.assignments = {}
         self.num_assns = 0
         self.scores = []
+        self.average_score = 0
+        self.total_score = 0
 
     def add_assignment(self, assignment_name, score):
-        self.assignments[assignment_name] = scores
+        self.assignments[assignment_name] = score
         self.num_assns  += 1
         
-    def output_stats(self, assignments_dict):
+    def create_scores_list(self, assignments_dict):
         self.scores = list(assignment_dict.values())
 
-    def 
-# thePrimagen/keyboards
+    def sum_scores(scores_list):
+        sum_total = 0
+        for i in range(0, len(scores_list)):
+            sum_total += scores_list[i]
+        self.total_score = sum_total
+
+    def compute_average(total_assignments, total_score):
+        self.average_score = total_assignments / total_score
+        
+for elem in list1:
+    compVar = '_' + str(elem[0]) 
+    eval(compVar) = new Student(elem[0], elem[1])
+    
+for elem in list2:
+    compVar = '_' + str(elem[0])
+    eval(compVar).add_assignment(elem[1], elem[2])
+
+header_string = ', '.join(outputHead) 
+with open('grades.txt', 'w') as f:
+    f.write(header_string)
+    f.close()
+
+for elem in list1:
+    output_list = []
+    compVar = '_' + str(elem[0])
+    vr = eval(compVar)
+    output_list.append(str(vr.id))
+    output_list.append(str(vr.name))
+    output_list.append(str(vr.create_scores_list(vr.assignments)))
+    output_list.append(str(vr.sum_scores(vr.scores)))
+    output_list.append(str(vr.compute_average(vr.num_assns, vr.total_score)))
+    output_line  = ', '.join(output_list)
+    with open('grades.txt', 'a') as f:
+        f.write(output line)
+        f.close()
